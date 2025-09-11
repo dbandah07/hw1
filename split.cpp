b5e0e47f4f26143018123617a3ad4413b5096048
Daniela Banda-Hernandez <bandaher@usc.edu>
2025-09-11 02:46:47 +0000

    all done with p1

--- split.cpp
+++ split.cpp
@@ -4,20 +4,74 @@ CSCI 104: Homework 1 Problem 1
 Write a recursive function to split a sorted singly-linked
 list into two sorted linked lists, where one has the even 
 numbers and the other contains the odd numbers. Students 
-will receive no credit for non-recursive solutions. 
+will receive no credit [] non-recursive solutions. 
 To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
 
-/* Add a prototype for a helper function here if you need */
+using namespace std;
+
+/* Add a prototype []] a helper function here if you need */
+
+// struct Node {
+//  int value;
+//  Node *next;
+//};
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  // split the elements of a sorted(in inc. order), 
+  // singly-linked lists of ints into TWO sorted, 
+  // singly linked lists where the first list are all the odd's 
+  // and the 2nd are all the even val's.
+  // og list IS NOT preserved
+
+  // cout << "DEBUG: made it to split" << endl;
+  if (in == NULL) {
+    return;
+  }
+
+  Node* temp = in; // current 
+  in = in->next; // going to nxt Node
+  temp->next = NULL; 
+
+  // split to evens
+  if (temp->value % 2 == 0) { 
+    // cout << "DEBUG: made it to mod 2 if" << endl;
+    // cout << temp->value << endl;
+    
+    if (evens == NULL) { // empty
+      // cout << "DEBUG: made it to evens == NULL" << endl;
+      evens = temp; // add even value
+      // cout << "DEBUG: evens->value " << evens->value << endl; 
+    }
+    else {
+      temp->next = evens; 
+      evens = temp; 
+    }
+  }
+
+  // split to odds
+  else {
+    if (odds == NULL) {
+      // cout << "DEBUG: made it to odds == NULL" << endl;
+      odds = temp;
+      // cout << "DEBUG: odds->value" << odds->value << endl;
+    }
+    else {
+      temp->next = odds;
+      odds = temp; 
+    }
+  }
+
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,64 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  Node* iter = new Node (0, NULL);
+  Node* head = iter;
 
+  for (int i = 1; i <= 12; i++) { // loops thru 12 
+    iter->next = new Node (i, nullptr); // adds pointer to iter 
+    iter = iter->next; 
+    // should print 12...0;
+  }
+
+  cout << "TESTING: all contents in loop" << endl;
+  // testing printing:
+  Node* print = head; // protect head;
+  while (print != nullptr) { // until it reaches end of list
+    cout << print->value << endl;
+    print = print->next;
+  }
+
+  // testing split ();
+  
+  cout << endl; 
+  cout << "TESTING: split function" << endl;
+  Node* t_evens = NULL; 
+  Node* t_odds = NULL;
+  // cout << "DEBUG: iter->value " << iter->value << endl;
+  // cout << "DEBUG: head->value" << head->value << endl;
+  split(head, t_odds, t_evens);
+
+
+
+  cout << endl;
+  cout << "TESTING: t_evens in split()" << endl;
+
+  Node* temp_e = t_evens;
+  Node* traverse;
+  // cout << ""DEBUG: temp_e->value" temp_e->value << endl; 
+  while (temp_e != nullptr) {
+    // cout << "DEBUG: makes it to while loop for temp_e" << endl;
+    cout << temp_e->value << endl; // print val in t_evens
+    traverse = temp_e;
+    temp_e = temp_e->next;
+    delete traverse;
+  }
+  cout << endl; 
+  cout << "TESTING: t_odds in split()" << endl;
+
+  Node* temp_o = t_odds;
+  while (temp_o != nullptr) {
+    cout << temp_o->value << endl;
+    traverse = temp_o;  
+    temp_o = temp_o->next;
+    delete traverse;
+  }
 }
